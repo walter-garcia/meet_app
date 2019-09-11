07bd35d050a6a9c6526a5bdaf65f8f84edb37b88
Walter Marcel Barbosa Garcia <waltermbgarcia@hotmail.com>
2019-09-11 10:06:38 -0300

    first commit

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,86 @@
+import * as Yup from 'yup';
+import User from '../models/User';
+
+class UserController {
+  async store(request, response) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .required()
+        .min(6),
+    });
+
+    if (!(await schema.isValid(request.body))) {
+      return response.status(400).json({ error: 'Validation failed' });
+    }
+
+    const checkUser = await User.findOne({
+      where: { email: request.body.email },
+    });
+
+    if (checkUser) {
+      return response.status(400).json({ error: 'User already exists.' });
+    }
+
+    const { id, name, email } = await User.create(request.body);
+
+    return response.json({
+      id,
+      name,
+      email,
+    });
+  }
+
+  async update(request, response) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    if (!(await schema.isValid(request.body))) {
+      return response.status(400).json({ error: 'Validation failed' });
+    }
+
+    const { email, oldPassword } = request.body;
+
+    const user = await User.findByPk(request.userId);
+
+    if (email && email !== user.email) {
+      const checkUser = await User.findOne({ where: { email } });
+
+      if (checkUser) {
+        return response.status(400).json({
+          error: 'There is already a registered user with this email',
+        });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return response
+        .status(401)
+        .json({ error: 'Old password does not match' });
+    }
+
+    const { id, name, email: userEmail } = await user.update(request.body);
+
+    return response.json({
+      id,
+      name,
+      email: userEmail,
+    });
+  }
+}
+
+export default new UserController();
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '8392f1ae9992d2adc76f05ddf15d6439',
+  expiresIn: '7d',
+};
